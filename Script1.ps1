a4860a4f4fc8efd69edc26585e6de01685677a82
aurehmaan <71068738+aurehmaan@users.noreply.github.com>
2022-04-09 08:52:30 +0500

    Update Script1.ps1

--- Script1.ps1
+++ Script1.ps1
@@ -6,4 +6,4 @@ Add-WindowsFeature Web-Server
 
 # Create file Default.htm and add some sample text
 
-Set-Content -Path C:\inetpub\wwwroot\Default.htm -Value "Hello Sir! This Machine is in Azure East US Zone. Computer Name is $($env:computername) " 
+Set-Content -Path C:\inetpub\wwwroot\Default.htm -Value "This Machine is in Azure East US Zone. Computer Name is $($env:computername) " 
